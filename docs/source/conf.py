f83c4350c7d1b13a53b36b470113122787949b2e
zhanghaoyang0 <zhanghaoyang0@hotmail.com>
2023-12-02 14:56:32 +0100

    first

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,6 @@ release = '202312'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ['sphinx_rtd_theme']
-
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -23,5 +22,6 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
