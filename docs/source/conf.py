6defb6465e17b63bc8fa44f532bc6ab3b86f30bb
zhanghaoyang0 <zhanghaoyang0@hotmail.com>
2023-12-02 14:42:52 +0100

    first

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '202312'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = ['python_docs_theme']
 
 templates_path = ['_templates']
 exclude_patterns = []
